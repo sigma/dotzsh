8806e85f386822e50cf81e487e2f8ee18e9b824d
Yann Hodique <hodiquey@vmware.com>
2016-10-29 18:43:11 -0700

    use highlight first

--- .zsh/less.plugin.zsh
+++ .zsh/less.plugin.zsh
@@ -7,8 +7,8 @@
 # Remove -X and -F (exit if the content fits on one screen) to enable it.
 export LESS='-F -g -i -M -R -S -w -X -z-4'
 
-# Set the Less input preprocessor.
-# Try both `lesspipe` and `lesspipe.sh` as either might exist on a system.
-if (( $#commands[(i)lesspipe(|.sh)] )); then
-  export LESSOPEN="| /usr/bin/env $commands[(i)lesspipe(|.sh)] %s 2>&-"
+if (( $#commands[highlight] )); then
+    export LESSOPEN="| $commands[highlight] %s --out-format xterm256 --line-numbers --quiet --force --style solarized-dark"
+elif (( $#commands[(i)lesspipe(|.sh)] )); then
+    export LESSOPEN="| /usr/bin/env $commands[(i)lesspipe(|.sh)] %s 2>&-"
 fi
